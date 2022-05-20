e67f1fbfff611e539bb2481d8d441a6374bf72bc
artneto <arthur_neto@icloud.com>
2022-05-20 07:23:20 +0000

    added basic payment  functionallity

--- checkout/views.py
+++ checkout/views.py
@@ -1,11 +1,11 @@
 from django.shortcuts import render, redirect, reverse
 from django.contrib import messages
+from django.conf import settings
 
 from .forms import OrderForm
 from bag.contexts import bag_contents
 
 import stripe
-
 
 
 def checkout(request):
@@ -26,14 +26,17 @@ def checkout(request):
         currency=settings.STRIPE_CURRENCY,
     )
 
-
     order_form = OrderForm()
+
+    if not stripe_public_key:
+        messages.warning(request, 'Stripe public key is missing. \
+            Did you forget to set it in your environment?')
+
     template = 'checkout/checkout.html'
     context = {
         'order_form': order_form,
-        'stripe_public_key':'pk_test_51L1586I64LPJhMAoNkv51tuf7aeEtZpTaERGtGBQ9lETJ2ibpTzUAcUEwBca3i4zbPYTHTOZOmyON6G6NaFq5y2H00uXhbp93C',
-        'client_secret': 'test client secret',
-
+        'stripe_public_key': stripe_public_key,
+        'client_secret': intent.client_secret,
     }
 
     return render(request, template, context)
