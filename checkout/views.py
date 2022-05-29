9d9016c68e9aad7415b2f61075a4a863032f93a7
artneto <arthur_neto@icloud.com>
2022-05-29 16:15:55 +0000

    added static files S3

--- checkout/views.py
+++ checkout/views.py
@@ -74,7 +74,6 @@ def checkout(request):
                                 order=order,
                                 product=product,
                                 quantity=quantity,
-                                product_size=size,
                             )
                             order_line_item.save()
                 except Product.DoesNotExist:
