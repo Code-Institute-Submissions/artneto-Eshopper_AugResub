f4d71a860d22f5254a1c0e9d266b937aa22110c8
artneto <arthur_neto@icloud.com>
2022-06-20 19:11:48 +0000

    added content_processor media line for the app_dirs

--- eshopper_store/settings.py
+++ eshopper_store/settings.py
@@ -84,6 +84,7 @@ TEMPLATES = [
                 'django.template.context_processors.request', # required by allauth
                 'django.contrib.auth.context_processors.auth',
                 'django.contrib.messages.context_processors.messages',
+                'django.template.context_processors.media',
                 'bag.contexts.bag_contents',
                 'social_django.context_processors.backends',
                 'social_django.context_processors.login_redirect',
