1d19103bd1943ba1f883f2389854325e8e0d0e54
artneto <arthur_neto@icloud.com>
2022-07-30 21:17:25 +0000

    create blog session for posts

--- blog/apps.py
+++ blog/apps.py
@@ -1,6 +1,6 @@
 from django.apps import AppConfig
 
 
-class LifestyleConfig(AppConfig):
+class BlogConfig(AppConfig):
     default_auto_field = 'django.db.models.BigAutoField'
-    name = 'lifestyle'
+    name = 'blog'
