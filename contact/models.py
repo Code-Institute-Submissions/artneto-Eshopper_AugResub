d108308eda32feecdfa95a47673f83615c221ae0
artneto <arthur_neto@icloud.com>
2022-08-05 22:04:51 +0000

    change modelform in forms py for contact us

--- contact/models.py
+++ contact/models.py
@@ -3,7 +3,7 @@ from django.db import models
 
 class Contact(models.Model):
     email = models.EmailField(max_length=254, null=True, blank=False)
-    subject = models.CharField(max_length=250, null=False, default='Add subject here', unique=True)
+    subject = models.CharField(max_length=250, null=False, default='your comment here', unique=True)
     message = models.TextField(null=False, default='Add message here')
 
     def __str__(self):
