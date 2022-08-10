9ed89e26b23ae2995d6ffbbc5981eeb4a698a825
artneto <arthur_neto@icloud.com>
2022-08-10 16:46:25 +0000

    adjusting files to pass the pep8 validation

--- bag/tests.py
+++ bag/tests.py
@@ -1,8 +1,3 @@
-from django.shortcuts import render
+from django.test import TestCase
 
-# Create your views here.
-
-def view_bag(request):
-    """ View that renders the bag contents page """
-
-    return render(request, 'bag/bag.html')
+# Create your tests here.
