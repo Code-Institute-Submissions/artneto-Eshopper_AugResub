02b197bd8826a8bf13db3e4df9d181f4df337d96
artneto <arthur_neto@icloud.com>
2022-08-10 18:49:10 +0000

    creating test forms for contact app

---
+++ contact/test_forms.py
@@ -0,0 +1,29 @@
+from django.test import TestCase
+from .forms import ContactForm
+
+
+class TestItemForm(TestCase):
+
+    def test_item_name_is_required(self):
+        form = ContactForm({'name': ''})
+        self.assertFalse(form.is_valid())
+        self.assertIn('name', form.errors.keys())
+        self.assertEqual(form.errors['name'][0], 'This field is required.')
+
+    def test_email_address_is_required(self):
+        form = ContactForm({'email': ''})
+        self.assertFalse(form.is_valid())
+        self.assertIn('email', form.errors.keys())
+        self.assertEqual(form.errors['email'][0], 'This field is required.')
+
+    def test_subject_is_required(self):
+        form = ContactForm({'subject': ''})
+        self.assertFalse(form.is_valid())
+        self.assertIn('subject', form.errors.keys())
+        self.assertEqual(form.errors['subject'][0], 'This field is required.')
+
+    def test_message_required(self):
+        form = ContactForm({'message': ''})
+        self.assertFalse(form.is_valid())
+        self.assertIn('message', form.errors.keys())
+        self.assertEqual(form.errors['subject'][0], 'This field is required.')
